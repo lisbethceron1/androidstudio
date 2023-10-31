1eab639ed20ec429947c84d437b8029d21c69599
valeriafern <vfernandez321@misena.edu.co>
2023-10-30 20:58:39 -0500

    botones listos

--- app/src/main/java/com/example/myapplication/MainActivity2.kt
+++ app/src/main/java/com/example/myapplication/MainActivity2.kt
@@ -1,11 +1,32 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.example.myapplication.databinding.ActivityMain2Binding
+import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity2 : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMain2Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+        binding =  ActivityMain2Binding.inflate(LayoutInflater.from(this))
+        setContentView(binding.root)
+
+        initUI()
+    }
+    private fun initUI(){
+        setupOnClicklistener()
+    }
+
+    private fun setupOnClicklistener(){
+        binding.btnd.setOnClickListener{
+            val intent = Intent (this,MainActivity3::class.java)
+
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/myapplication/MainActivity3.kt
+++ app/src/main/java/com/example/myapplication/MainActivity3.kt
@@ -3,20 +3,16 @@ package com.example.myapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.AbsSavedState
 import android.view.LayoutInflater
-import android.widget.Button
-import android.widget.Toast
-import com.example.myapplication.databinding.ActivityMainBinding
+import com.example.myapplication.databinding.ActivityMain2Binding
+import com.example.myapplication.databinding.ActivityMain3Binding
 
-class MainActivity : AppCompatActivity() {
-
-private lateinit var binding: ActivityMainBinding
-
+class MainActivity3 : AppCompatActivity() {
+    private lateinit var binding: ActivityMain3Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        binding =  ActivityMainBinding.inflate(LayoutInflater.from(this))
+        setContentView(R.layout.activity_main3)
+        binding =  ActivityMain3Binding.inflate(LayoutInflater.from(this))
         setContentView(binding.root)
 
         initUI()
@@ -26,8 +22,8 @@ private lateinit var binding: ActivityMainBinding
     }
 
     private fun setupOnClicklistener(){
-        binding.btn.setOnClickListener{
-            val intent = Intent (this,MainActivity2::class.java)
+        binding.btnn.setOnClickListener{
+            val intent = Intent (this,MainActivity::class.java)
 
             startActivity(intent)
         }
